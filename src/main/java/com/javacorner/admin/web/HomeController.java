cf107bf2a919872c732d4f84e67a24e81b9c9108
vakser <65089446+vakser@users.noreply.github.com>
2023-04-29 11:11:32 +0300

    Correct authentication for home page

--- src/main/java/com/javacorner/admin/web/HomeController.java
+++ src/main/java/com/javacorner/admin/web/HomeController.java
@@ -1,11 +1,13 @@
 package com.javacorner.admin.web;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
     @GetMapping("/")
+    @PreAuthorize("hasAnyAuthority('Admin', 'Instructor', 'Student')")
     public String home() {
         return "home";
     }
